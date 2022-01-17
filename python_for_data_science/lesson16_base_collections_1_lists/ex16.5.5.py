14fa60b351a15f7334fd4c24abdd0541a5270149
Gegcuk <gegcuk@gmail.com>
2022-01-17 17:40:57 +0300

    ex16.5.5 done

--- python_for_data_science/lesson16_base_collections_1_lists/ex16.5.5.py
+++ python_for_data_science/lesson16_base_collections_1_lists/ex16.5.5.py
@@ -17,4 +17,17 @@
 # Ошибка: фильма Безумный Макс у нас нет :(#
 # Введите название фильма: Мементо#
 # Ваш список любимых фильмов: Леон, Мементо
-#
+
+
+films = ['Крепкий орешек', 'Назад в будущее', 'Таксист', 'Леон', 'Богемская рапсодия', 'Город грехов', 'Мементо', 'Отступники', 'Деревня']
+favorite_films = []
+counts_of_favorite_films = int(input("How many films do you want to add?: "))
+for _ in range(counts_of_favorite_films):
+    film = input("Insert film`s title: ")
+    if film in films:
+        favorite_films.append(film)
+        continue
+    else:
+        print('Фильма {} у нас нет =('.format(film))
+
+print('Ваш список любимых фильмов: {}'.format(', '.join(favorite_films)))
