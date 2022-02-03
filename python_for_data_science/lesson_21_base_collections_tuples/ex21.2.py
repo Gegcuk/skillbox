1522c3b77160a913a8b4f50f840ae8775b21844c
Aleksey Lazunin <gegcuk@gmail.com>
2022-02-03 19:05:20 +0300

    ex21.2.2 done

--- python_for_data_science/lesson_21_base_collections_tuples/ex21.2.py
+++ python_for_data_science/lesson_21_base_collections_tuples/ex21.2.py
@@ -1,3 +1,7 @@
+import random
+import string
+
+
 def ex21_2_1():
     # Задача 1. Саботаж!
     # Какой-то нехороший человек решил подпортить жизнь frontend-разработчикам и добавил в код сайта символ
@@ -14,5 +18,27 @@ def ex21_2_1():
     for index, value in enumerate(user_text):
         if value == '~':
             print(index, end=' ')
+
+
+def ex21_2_2():
+    # Задача 2. Словари из списков
+    # Создайте два списка, в каждом из которых лежит 10 случайных букв алфавита (могут повторяться). Затем для каждого
+    # списка создайте словарь из пар «индекс — значение» и выведите оба словаря на экран.
+    #
+    # Подсказка: random
+    #
+    # Пример:
+    # Первый список: ['й', 'р', 'с', 'г', 'а', 'а', 'т', 'ж', 'е', 'к']
+    # Второй список: ['д', 'а', 'а', 'в', 'т', 'ж', 'р', 'б', 'й', 'р']
+    #
+    # Первый словарь: {0: 'й', 1: 'р', 2: 'с', 3: 'г', 4: 'а', 5: 'а', 6: 'т', 7: 'ж', 8: 'е', 9: 'к'}
+    # Второй словарь: {0: 'д', 1: 'а', 2: 'а', 3: 'в', 4: 'т', 5: 'ж', 6: 'р', 7: 'б', 8: 'й', 9: 'р'}
+    letters = string.ascii_lowercase
+    first_list = [random.choice(letters) for _ in range(10)]
+    second_list = [random.choice(letters) for _ in range(10)]
+    first_dict = {index: value for index, value in enumerate(first_list)}
+    second_dict = {index: value for index, value in enumerate(second_list)}
+    print(first_dict, '\n', second_dict)
+
 
 
