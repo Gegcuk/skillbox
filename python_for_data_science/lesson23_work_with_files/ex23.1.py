cd9994dd23d4062b39cbe245815e1c958f3bafa3
Aleksey Lazunin <gegcuk@gmail.com>
2022-02-08 12:53:34 +0300

    ex23.1 done

--- python_for_data_science/lesson23_work_with_files/ex23.1.py
+++ python_for_data_science/lesson23_work_with_files/ex23.1.py
@@ -1,14 +1,19 @@
 import os
 
 
-def print_dirs(project):
-    print('\nContent of folder: ', project)
-    for i_elem in os.listdir(project):
-        path = os.path.join(project, i_elem)
-        print('    ', path)
+def ex23_1_0():
+    def print_dirs(project):
+        print('\nContent of folder: ', project)
+        if os.path.exists(project):
+            for i_elem in os.listdir(project):
+                path = os.path.join(project, i_elem)
+                print('    ', path)
+        else:
+            print('Directory is not exist')
+
+    projects_list = ['skillbox', 'probe', 'testProject1']
+    for i_project in projects_list:
+        path_to_project = os.path.abspath(os.path.join('..', '..', '..', i_project))
+        print_dirs(path_to_project)
 
 
-projects_list = ['skillbox', 'probe', 'testProject1']
-for i_project in projects_list:
-    path_to_project = os.path.abspath(os.path.join('..', '..', '..', i_project))
-    print_dirs(path_to_project)
