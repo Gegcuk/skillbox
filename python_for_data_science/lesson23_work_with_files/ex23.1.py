f840d88d872f7e35319dfb1d87c1720c7f3e4bb9
Aleksey Lazunin <gegcuk@gmail.com>
2022-02-08 18:27:24 +0300

    ex23.1.2 done

--- python_for_data_science/lesson23_work_with_files/ex23.1.py
+++ python_for_data_science/lesson23_work_with_files/ex23.1.py
@@ -51,4 +51,32 @@ def ex23_1_1():
         print('File is not exist')
 
 
-
+def ex23_1_2():
+    # Задача 2. Поиск файла
+    # В уроке мы написали функцию, которая ищет нужный нам файл во всех подкаталогах указанной директории. Однако, как
+    # мы понимаем, файлов с таким названием может быть несколько.
+    # Напишите функцию, которая принимает на вход абсолютный путь до директории и имя файла, проходит по всем вложенным
+    # файлам и папкам и выводит на экран все абсолютные пути с этим именем.
+    #
+    # Пример:
+    # Ищем в: C:/Users/Roman/PycharmProjects/Skillbox
+    # Имя файла: lesson2
+    #
+    # Найдены следующие пути:
+    # C:/Users/Roman/PycharmProjects/Skillbox\Module15\lesson2.py
+    # C:/Users/Roman/PycharmProjects/Skillbox\Module16\lesson2.py
+    # C:/Users/Roman/PycharmProjects/Skillbox\Module17\lesson2.py
+    # C:/Users/Roman/PycharmProjects/Skillbox\Module18\lesson2.py
+    # C:/Users/Roman/PycharmProjects/Skillbox\Module19\lesson2.py
+    # C:/Users/Roman/PycharmProjects/Skillbox\Module20\lesson2.py
+    # C:/Users/Roman/PycharmProjects/Skillbox\Module21\lesson2.py
+    # C:/Users/Roman/PycharmProjects/Skillbox\Module22\lesson2.py
+    path = input('Enter path: ')
+    file_name = input('Enter file name: ')
+    os.chdir(os.path.abspath(path))
+    all_files = []
+    for root, dirs, files in os.walk(".", topdown=False):
+        for name in files:
+            if os.path.basename(name) == file_name:
+                all_files.append(os.path.abspath(name))
+    print(all_files)
