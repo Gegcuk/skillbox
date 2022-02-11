cf20a7b8b6a06a06e0bd0c5d013e54aaa64a6333
Aleksey Lazunin <gegcuk@gmail.com>
2022-02-11 13:25:51 +0300

    ex25.0 done

---
+++ python_for_data_science/lesson25_intro_to_OOP/ex25.0.py
@@ -0,0 +1,18 @@
+
+
+user_name = input("Enter your name: ")
+while True:
+    response = input('Enter 1 or 2. If you want to look messages - 1, if you want to write - 2')
+    if response == '1':
+        try:
+            with open('chat.txt', 'r') as file:
+                messages = file.readlines()
+                print(''.join(messages))
+        except FileNotFoundError:
+            print('File doesn`t exist.')
+    elif response == '2':
+        new_message = input('Enter your message: ')
+        with open('chat.txt', 'a') as file:
+            file.write(f'{user_name}: {new_message}\n')
+    else:
+        print('Unknown command.')
