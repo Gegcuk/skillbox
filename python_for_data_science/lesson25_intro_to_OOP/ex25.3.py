eb4e67828b055c13e65c62a8246b445f5b0e6bee
Aleksey Lazunin <gegcuk@gmail.com>
2022-02-15 17:52:12 +0300

    ex25.3.2 done

--- python_for_data_science/lesson25_intro_to_OOP/ex25.3.py
+++ python_for_data_science/lesson25_intro_to_OOP/ex25.3.py
@@ -44,3 +44,57 @@ def ex25_3_1():
     third_toyota.info()
 
 
+def ex25_3_2():
+    # Задача 2. Семья
+    #
+    # Реализуйте класс «Семья», который состоит из атрибутов «Имя», Деньги» и «Наличие дома» и объекты которого могут выполнять следующие действия:
+    #
+    # Отобразить информацию о себе.
+    # Заработать денег (подаётся число, которое прибавляется к текущему значению денег).
+    # Купить дом (подаётся стоимость дома и необязательный аргумент «Скидка»). Вывести соответствующее сообщение об успешной/неуспешной покупке дома.
+    # Создайте как минимум один экземпляр класса и проверьте работу методов.
+    #
+    # Пример работы программы (вывод информации, покупка дома, заработок, очередная покупка):
+    #
+    # Family name: Common family
+    # Family funds: 100000
+    # Having a house: False
+    #
+    # Try to buy a house
+    # Not enough money!
+    #
+    # Earned 800000 money! Current value: 900000
+    # Try to buy a house again
+    # House purchased! Current money: 0.0
+    #
+    # Family name: Common family
+    # Family funds: 0.0
+    # Having a house: True
+    class Family:
+        name = 'Simpsons'
+        funds = 100000
+        with_home = False
+
+        def info(self):
+            print('Family name: {}\nFamily funds: {}\nHas house? {}'.format(self.name,
+                                                                            self.funds,
+                                                                            self.with_home))
+
+        def earn_money(self, earned_money):
+            self.funds += earned_money
+
+        def buy_house(self, price, discount=0):
+            price = price - price * discount / 100
+            if price > self.funds:
+                print('You cannot buy house.')
+            else:
+                print('You have house!')
+                self.funds -= price
+                self.with_home = True
+
+    family_1 = Family()
+    family_1.info()
+    family_1.buy_house(200000)
+    family_1.earn_money(101000)
+    family_1.buy_house(200000, 10)
+    family_1.info()
