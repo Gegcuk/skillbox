7a1233c976d13abae0f7365432c8aa3b75519dd7
Aleksey Lazunin <gegcuk@gmail.com>
2022-02-17 11:51:48 +0300

    ex25.4.3 done

--- python_for_data_science/lesson25_intro_to_OOP/ex25.4.py
+++ python_for_data_science/lesson25_intro_to_OOP/ex25.4.py
@@ -76,3 +76,50 @@ def ex25_4_2():
     point_7 = Point(10, 20)
     point_8 = Point(10, 20)
     point_8.info()
+
+
+class Potato:
+    states = {0: 'Planted', 1: 'Sprout', 2: 'Green', 3: 'Ripe'}
+
+    def __init__(self, index):
+        self.index = index
+        self.state = 0
+
+    def grow(self):
+        if self.state <3:
+            self.state += 1
+        self.print_state()
+
+    def is_ripe(self):
+        if self.state == 3:
+            return True
+        return False
+
+    def print_state(self):
+        print('Potato {} is {}'.format(self.index, Potato.states[self.state]))
+
+
+class PotatoGarden:
+
+    def __init__(self, count):
+        self.potatoes = [Potato(index) for index in range (1, count+1)]
+
+    def grow_all(self):
+        print('Potato is growing!')
+        for i_potato in self.potatoes:
+            i_potato.grow()
+
+    def are_all_ripe(self):
+        if not all([i_potato.is_ripe() for i_potato in self.potatoes]):
+            print('Potato is not ready')
+        else:
+            print('All potatoes are grown!')
+
+
+my_garden = PotatoGarden(5)
+my_garden.are_all_ripe()
+my_garden.grow_all()
+my_garden.grow_all()
+my_garden.grow_all()
+my_garden.grow_all()
+my_garden.are_all_ripe()
