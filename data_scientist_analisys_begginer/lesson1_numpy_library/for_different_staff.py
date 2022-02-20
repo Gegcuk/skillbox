2176ec02efb977a22f55d49049564aec2ba2fe8c
Aleksey Lazunin <gegcuk@gmail.com>
2022-02-21 01:01:29 +0300

    different test staff with arrays

--- data_scientist_analisys_begginer/lesson1_numpy_library/for_different_staff.py
+++ data_scientist_analisys_begginer/lesson1_numpy_library/for_different_staff.py
@@ -7,3 +7,13 @@ print(np.log(np.e))
 print(np.mean([20, 13, 20, 30, 40, 23, 40, 1000]))
 
 print(np.median([20, 13, 20, 30, 40, 23, 40, 1000]))
+
+test_list = [20, 13, 20, 30, 40, 23, 40, 1000]
+
+test_array = np.array(test_list)
+
+test_array_family_size = np.array([2, 3, 4])
+
+test_array_persons_raiting = np.array([[5, 2, 3.5], [1, 9, 6.5]])
+
+print('{}\n{}\n{}'.format(test_array, test_array_family_size, test_array_persons_raiting))
