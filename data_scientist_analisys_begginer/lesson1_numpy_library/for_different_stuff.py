f672b320d5e53fe4bed7b447837c4d397fdaf37e
Aleksey Lazunin <gegcuk@gmail.com>
2022-02-22 11:28:28 +0300

    hw1 done

--- data_scientist_analisys_begginer/lesson1_numpy_library/for_different_stuff.py
+++ data_scientist_analisys_begginer/lesson1_numpy_library/for_different_stuff.py
@@ -17,3 +17,17 @@ test_array_family_size = np.array([2, 3, 4])
 test_array_persons_raiting = np.array([[5, 2, 3.5], [1, 9, 6.5]])
 
 print('{}\n{}\n{}'.format(test_array, test_array_family_size, test_array_persons_raiting))
+
+turnout = np.array([0.62, 0.43, 0.80, 0.56])
+print(turnout * 100)
+
+m = np.array([[2, 5], [6, 8], [1, 3]])
+
+print(m.ndim)
+
+print(m.shape)
+
+print(m.size)
+
+print(m[1][0])
+
